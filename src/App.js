9fc8959dedd4753cd9e367e709511d401e9fdcbc
dreambeyondit <dreambeyondit@tutanota.com>
2020-03-24 14:55:53 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='text-4xl font-bold text-center text-blue-500'>
+      React + Tailwind CSS
+    </div>
+  );
+}
+
+export default App;
